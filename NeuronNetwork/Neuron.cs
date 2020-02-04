7bf270c55231d23b8da23b6be603ea60b7963039
ivan.nichipor <ivan.nichipor@ESW568>
2020-02-04 10:31:11 +0300

    Neuron adding

---
+++ NeuronNetwork/Neuron.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NeuronNetwork
+{
+    public class Neuron
+    {
+        public List<double> Weights { get; set; }
+
+        public NeuronType NeuronType { get; set; }
+
+        public double Output { get; private set; }
+
+        public Neuron(int inputCount, NeuronType type = NeuronType.Normal)
+        {
+            NeuronType = type;
+
+            Weights = new List<double>();
+
+            for (int i = 0; i < inputCount; i++)
+            {
+                Weights.Add(i);
+            }
+        }
+
+        public double FeedForward(List<double> input)
+        {
+            var sum = 0.0;
+
+            for (int i = 0; i < input.Count; i++)
+            {
+                sum += input[i] * Weights[i];
+            }
+
+            Output = Signoid(sum);
+
+            return Output;
+        }
+
+        public double Signoid(double x)
+        {
+            var result = 1 / (1 - Math.Pow(Math.E, -x));
+
+            return result;
+        }
+
+        public override string ToString()
+        {
+            return Output.ToString();
+        }
+    }
+}
